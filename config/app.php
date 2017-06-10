17ae5cd0fab7b1cbb244432c57c3bfa379b75e0c
Carlos <anjoazrael10@gmail.com>
2017-06-10 11:09:15 -0300

    atualizando pgsql com dados da heroku

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/public'),
+    'url' => env('APP_URL', 'http://secure-hollows-13721.herokuapp.com/public/'),
 
     /*
     |--------------------------------------------------------------------------
