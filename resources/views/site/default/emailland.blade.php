43bd91ce0df333734827329193b5986da3b9f050
Carlos <anjoazrael10@gmail.com>
2017-06-13 16:45:59 -0300

    corrigindo alert info de escolha de email ou QRCode

--- resources/views/site/default/emailland.blade.php
+++ resources/views/site/default/emailland.blade.php
@@ -1,7 +1,5 @@
 <div class='container text-center' >
-  <div style='padding:12px'> 
-    <span class='alert alert-info' >Scanneie o QRCode para se inscrever em nossa lista de ofertas ou click no botao</span>
-  </div>
+    <p class='alert alert-info' >Scanneie o QRCode para se inscrever em nossa lista de ofertas ou click no botao</p>
     <div class='col-lg-4 col-lg-offset-4 panel-body'>
       <a href='http://eepurl.com/cSmZpr' class='btn btn-lg btn-success' target="_blank">Quero Receber as Ofertas</a>
       <img class='img-responsive center-block' src='{{asset("img/QR/cSmZpr.qr.16.jpg")}}' width="250" />
