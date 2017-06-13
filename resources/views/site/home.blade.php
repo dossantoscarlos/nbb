929ba6503e23237d243e9fc4e513fdc863f32c67
Carlos <anjoazrael10@gmail.com>
2017-06-13 15:34:27 -0300

    add popup

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -15,5 +15,12 @@
   @component('site.homeComponents.listhome') @endcomponent
   @component('site.default.emailland') @endcomponent
   @component('site.default.servicos') @endcomponent
-
+  <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" 
+  data-dojo-config="usePlainJson: true, isDebug: false"></script>
+  <script type="text/javascript">
+    require(["mojo/signup-forms/Loader"], 
+            function(L) { 
+              L.start({"baseUrl":"mc.us16.list-manage.com","uuid":"d687c905f7cfe6cd13f3dcc46","lid":"f8b7dfe175"});
+            });
+  </script>
 @endsection
