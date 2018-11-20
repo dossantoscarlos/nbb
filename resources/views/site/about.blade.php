ea60d485125f50d874cb9954fd80201ecf4ffe2c
dossantoscarlos <anjoazrael10@gmail.com>
2018-11-20 01:52:50 -0200

    add tag <p>

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -13,6 +13,7 @@
   <article class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
     <img src='{{ secure_asset('img/maxresdefault.jpg') }}' alt='minha foto' title='quem sou' class="img-responsive block-center img-circle">
   </article>
+  <p></p>
   <article class="col-lg-offset-1 col-lg-4 col-md-4 col-sm-12 col-xs-12 panel-body bg-info">
     <dl>
       <dt class='text-center' > Bem vindo a NBB </dt>
